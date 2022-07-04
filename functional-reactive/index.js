7eb2579b9dc632b5a8486a461054d9960c9fbaa5
lemontea <lemontea.Tom@gmail.com>
2022-07-04 11:11:25 +0000

    pull based wip

--- functional-reactive/index.js
+++ functional-reactive/index.js
@@ -104,12 +104,93 @@ class PushFRP {
         }
     }
 };
-var testNode1 = { isValueNode: true, val: 12 };
-var testNode2 = { isValueNode: true, val: 31 };
-var testNode3 = { isValueNode: false, fn: (v) => { const { test1, test2 } = v; return test1 + test2; } };
-var testNode4 = { isValueNode: true, val: 8 };
-var testNode5 = { isValueNode: false, fn: (v) => { const { test1, test3, test4 } = v; return test1 - test3*test4; } };;
-const myTest = new Map([
+
+class PullFRP {
+    //TODO: ctor copied from Push, merge?
+    constructor(nodes, edges) {
+        //this.nodes = nodes;
+        this.sortOp = new TopologicalSort(nodes);
+        edges.forEach((edge) => { this.sortOp.addEdge(edge[0], edge[1]); });
+        const sorted = this.sortOp.sort();
+        this.nodes = sorted;
+        edges.forEach((e) => {
+            const [from, to] = e;
+            const toNode = this.nodes.get(to);
+            if (!toNode.parents) toNode.parents = [];
+            toNode.parents.push(from);
+        });
+        this.sortedKeys = [...sorted.keys()];
+        this.sortedKeys.forEach((k, i) => { this.nodes.get(k).order = i; });
+
+        this.updatedNodes = new Set();
+    }
+
+    updateOne(n) {
+        const curNode = this.nodes.get(n);
+        if (curNode.node.isValueNode) return curNode.node.val;
+
+        console.log(curNode);
+
+        var dependNodes = curNode.parents.map((m) => {
+            return [m, this.nodes.get(m).order];
+        });
+        dependNodes.sort((a, b) => {
+            return a[1] - b[1];
+        });
+        console.log(dependNodes);
+        dependNodes.forEach((x) => {
+            const [n, _] = x;
+            this.updateOne(n);
+        });
+
+        var inputs = {};
+        curNode.parents.map((p) => {
+            const node = this.nodes.get(p).node;
+            const v = (node.isValueNode) ? node.val : node.cachedVal;
+            return [p, v];
+        }).forEach((x) => {
+            const [p, v] = x;
+            inputs[p] = v;
+        });
+
+        curNode.node.cachedVal = curNode.node.fn(inputs);
+        curNode.node.updated = true;
+
+        this.updatedNodes.add(n);
+
+        return curNode.node.cachedVal;
+    }
+
+    reset() {
+        this.updatedNodes.forEach((n) => {
+            this.nodes.get(n).node.updated = false;
+        })
+    }
+}
+
+//var extend = require('util')._extend;
+import { _extend as extend } from 'util';
+
+const testNode1_initial = { isValueNode: true, val: 12 };
+const testNode2_initial = { isValueNode: true, val: 31 };
+const testNode3_initial = { isValueNode: false, fn: (v) => { const { test1, test2 } = v; return test1 + test2; } };
+const testNode4_initial = { isValueNode: true, val: 8 };
+const testNode5_initial = { isValueNode: false, fn: (v) => { const { test1, test3, test4 } = v; return test1 - test3*test4; } };
+
+var testNode1 = extend({}, testNode1_initial);
+var testNode2 = extend({}, testNode2_initial);
+var testNode3 = extend({}, testNode3_initial);
+var testNode4 = extend({}, testNode4_initial);
+var testNode5 = extend({}, testNode5_initial);
+
+function reset_Tests() {
+    testNode1 = extend({}, testNode1_initial);
+    testNode2 = extend({}, testNode2_initial);
+    testNode3 = extend({}, testNode3_initial);
+    testNode4 = extend({}, testNode4_initial);
+    testNode5 = extend({}, testNode5_initial);
+}
+var myTest = new Map([
     ['test5', testNode5],
     ['test4', testNode4],
     ['test3', testNode3],
@@ -129,3 +210,18 @@ const what1 = o1.nodes;;
 testNode3.fn({ test1: 1, test2: 4 });
 o1.fullUpdate(['test4', 'test1']);;
 console.log(o1.nodes);
+
+reset_Tests();
+
+const o2 = new PullFRP(myTest, myTestEdges);
+//testNode1.val = -10;
+myTest.get('test1').val = -10;
+const ans2 = o2.updateOne('test5');
+console.log(o2.nodes);
+console.log("Answer is " + ans2);
+
+myTest.get('test4').val = 1;
+o2.reset();
+const ans3 = o2.updateOne('test5');
+console.log(o2.nodes);
+console.log("Answer is " + ans3);
